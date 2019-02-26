0b17fbaf61c81c54208daee0756b346b374294fc
Surma <surma@surma.link>
2019-02-26 14:07:01 +0000

    License fixes and stuff

--- src/actors/state/types.ts
+++ src/actors/state/types.ts
@@ -1,4 +1,17 @@
-import { Cell, State as GameState, Tag } from "../../gamelogic/types.js";
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Tag } from "../../gamelogic/types.js";
 
 import MinesweeperGame from "../../gamelogic/index.js";
 
@@ -24,7 +37,6 @@ export interface RequestStateMessage {
 }
 
 export interface StateMessage {
-  // state: State;
   state: MinesweeperGame;
 }
 
@@ -33,14 +45,4 @@ export type Message =
   | RevealFieldMessage
   | RequestStateMessage;
 
-// export interface State {
-//   gameState: GameState;
-//   grid: Cell[][];
-// }
-
 export type State = MinesweeperGame;
-
-// export const defaultState: State = {
-//   gameState: GameState.Pending,
-//   grid: [],
-// };
