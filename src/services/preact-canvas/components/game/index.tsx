a09da14e74eaeefa9e54acbc9c0cd3396aea0f8f
Jake Archibald <jaffathecake@gmail.com>
2019-04-01 17:36:53 +0100

    Adding flag button

---
+++ src/services/preact-canvas/components/game/index.tsx
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Remote } from "comlink/src/comlink";
+import PointerTracker from "pointer-tracker";
+import { Component, h } from "preact";
+import StateService from "src/services/state";
+import { bind } from "src/utils/bind";
+import { GridChangeSubscriptionCallback } from "../..";
+import { Cell } from "../../../../gamelogic/types";
+import Board from "../board";
+import { altLabel } from "./style.css";
+
+export interface Props {
+  stateService: Remote<StateService>;
+  grid: Cell[][];
+  gridChangeSubscribe: (f: GridChangeSubscriptionCallback) => void;
+}
+
+interface State {
+  altActionChecked: boolean;
+  altActionHeld: boolean;
+}
+
+export default class Game extends Component<Props, State> {
+  altLabel?: HTMLLabelElement;
+
+  state: State = {
+    altActionChecked: false,
+    altActionHeld: false
+  };
+
+  componentDidMount() {
+    const tracker: PointerTracker = new PointerTracker(this.altLabel!, {
+      end: () => {
+        this.setState({ altActionHeld: false });
+      },
+      start: (pointer, event) => {
+        if (tracker.currentPointers.length > 0) {
+          return false;
+        }
+        event.preventDefault();
+        this.setState({ altActionHeld: true });
+        return true;
+      }
+    });
+  }
+
+  render({ grid, gridChangeSubscribe }: Props, { altActionChecked }: State) {
+    return (
+      <div>
+        <Board
+          grid={grid}
+          gridChangeSubscribe={gridChangeSubscribe}
+          onCellClick={this.onCellClick}
+        />
+        <label class={altLabel} ref={el => (this.altLabel = el)}>
+          <input
+            type="checkbox"
+            onChange={this.onAltChange}
+            checked={altActionChecked}
+          />
+          Flag / reveal surrounding
+        </label>
+      </div>
+    );
+  }
+
+  @bind
+  private onAltChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.setState({
+      altActionChecked: target.checked
+    });
+  }
+
+  @bind
+  private onCellClick(cell: [number, number, string], forceAlt: boolean) {
+    const [x, y, state] = cell;
+    const { altActionChecked, altActionHeld } = this.state;
+
+    const altAction = forceAlt || altActionHeld || altActionChecked;
+
+    if (state === "unrevealed" && !altAction) {
+      this.props.stateService.reveal(x, y);
+    } else if (state === "unrevealed" && altAction) {
+      this.props.stateService.flag(x, y);
+    } else if (state === "flagged" && altAction) {
+      this.props.stateService.unflag(x, y);
+    } else if (Number(state) !== Number.NaN && altAction) {
+      this.props.stateService.revealSurrounding(x, y);
+    }
+  }
+}
