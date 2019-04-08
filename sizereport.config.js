7c76de48cb8b81fabee8b97d7d568c28ad875e0f
Jake Archibald <jaffathecake@gmail.com>
2019-04-08 11:26:37 -0400

    Size report (#22)
    * I wonder if this works
    
    * Let's try that again
    
    * hah wrong files
    
    * Updating size reporter, and making a demo change
    
    * Ready to go

---
+++ sizereport.config.js
@@ -0,0 +1,18 @@
+const { parse } = require("path");
+const rescapeRE = require("escape-string-regexp");
+
+module.exports = {
+  repo: "GoogleChromeLabs/graviton",
+  path: "dist/**/*",
+  branch: "master",
+  findRenamed(path, newPaths) {
+    const parsedPath = parse(path);
+    if (parsedPath.base.startsWith("bootstrap-")) {
+      const end = /\..*$/.exec(parsedPath.base)[0];
+      const re = new RegExp(
+        `^${rescapeRE(parsedPath.dir)}/bootstrap-[^\.]+${end}$`
+      );
+      return newPaths.find(path => re.test(path));
+    }
+  }
+};
