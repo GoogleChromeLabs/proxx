bfa14c583aaa03a5608d5c17fa8c83d43a29873b
Jake Archibald <jaffathecake@gmail.com>
2019-04-10 09:14:18 -0400

    Remove iterator (it's slow)

--- src/gamelogic/index.ts
+++ src/gamelogic/index.ts
@@ -107,7 +107,7 @@ export default class MinesweeperGame {
 
     if (tag === Tag.Flag) {
       this._flags++;
-      for (const [nextX, nextY] of this._iterateSurrounding(x, y)) {
+      for (const [nextX, nextY] of this._getSurrounding(x, y)) {
         const nextCell = this.grid[nextY][nextX];
         nextCell.touchingFlags++;
         // Emit this if it's just matched the number of mines
@@ -120,7 +120,7 @@ export default class MinesweeperGame {
       }
     } else if (oldTag === Tag.Flag) {
       this._flags--;
-      for (const [nextX, nextY] of this._iterateSurrounding(x, y)) {
+      for (const [nextX, nextY] of this._getSurrounding(x, y)) {
         const nextCell = this.grid[nextY][nextX];
         nextCell.touchingFlags--;
         // Emit this if it's just un-matched the number of mines
@@ -151,7 +151,7 @@ export default class MinesweeperGame {
 
     let revealedSomething = false;
 
-    for (const [nextX, nextY] of this._iterateSurrounding(x, y)) {
+    for (const [nextX, nextY] of this._getSurrounding(x, y)) {
       const nextCell = this.grid[nextY][nextX];
       if (nextCell.tag === Tag.Flag || nextCell.revealed) {
         continue;
@@ -216,10 +216,9 @@ export default class MinesweeperGame {
     this._state = State.Playing;
   }
 
-  private *_iterateSurrounding(
-    x: number,
-    y: number
-  ): IterableIterator<[number, number]> {
+  private _getSurrounding(x: number, y: number): Array<[number, number]> {
+    const surrounding: Array<[number, number]> = [];
+
     for (const nextY of [y - 1, y, y + 1]) {
       if (nextY < 0) {
         continue;
@@ -239,9 +238,11 @@ export default class MinesweeperGame {
           continue;
         }
 
-        yield [nextX, nextY];
+        surrounding.push([nextX, nextY]);
       }
     }
+
+    return surrounding;
   }
 
   /**
@@ -282,7 +283,7 @@ export default class MinesweeperGame {
       const maybeReveal: number[] = [];
 
       // Go around the surrounding squares
-      for (const [nextX, nextY] of this._iterateSurrounding(x, y)) {
+      for (const [nextX, nextY] of this._getSurrounding(x, y)) {
         const nextCell = this.grid[nextY][nextX];
 
         if (nextCell.hasMine) {
