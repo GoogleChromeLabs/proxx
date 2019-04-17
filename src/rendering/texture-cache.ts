183d6488622a342b96c574b31f95dfb52e6673d0
Jake Archibald <jaffathecake@gmail.com>
2019-04-17 17:15:04 +0100

    Short delay

--- src/rendering/texture-cache.ts
+++ src/rendering/texture-cache.ts
@@ -10,6 +10,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import { task } from "../utils/scheduling";
 import { staticDevicePixelRatio } from "../utils/static-dpr.js";
 import { TextureGenerator } from "./texture-generators.js";
 
@@ -94,7 +96,7 @@ export async function cacheTextureGenerator(
     image.src = URL.createObjectURL(blob);
     await new Promise(r => (image.onload = r));
     caches[idx] = image;
-    await new Promise(r => setTimeout(r, 500));
+    await task();
   }
 
   return (
