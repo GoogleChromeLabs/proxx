727f7aa3a1e01c25b8c688aaf64950f056e1e945
Jake Archibald <jaffathecake@gmail.com>
2019-04-24 15:15:57 +0100

    Initial service worker (#140)
    * Initial service worker
    
    * Don't cache bootstrap
    
    * bootstrap no longer has a hash.
    
    * Handing service worker updates and refactoring loading
    
    * Missed some bits
    
    * Adding constant

--- src/services/preact-canvas/index.tsx
+++ src/services/preact-canvas/index.tsx
@@ -21,8 +21,13 @@ import StateService from "../state/index.js";
 import localStateSubscribe from "../state/local-state-subscribe.js";
 import BottomBar from "./components/bottom-bar";
 import deferred from "./components/deferred";
+import GameLoading from "./components/game-loading";
 import Intro from "./components/intro/index.js";
 import { game as gameClassName, main } from "./style.css";
+
+// If the user tries to start a game when we aren't ready, how long do we wait before showing the
+// loading screen?
+const loadingScreenTimeout = 1000;
 
 interface Props {
   stateServicePromise: Promise<Remote<StateService>>;
@@ -30,10 +35,8 @@ interface Props {
 
 interface State {
   game?: GameType;
-  stateService?: Remote<StateService>;
   dangerMode: boolean;
-  texturesReady: boolean;
-  rendererReady: boolean;
+  awaitingGame: boolean;
 }
 
 export type GameChangeCallback = (stateChange: GameStateChange) => void;
@@ -48,39 +51,39 @@ const Game = deferred(
   import("./components/game/index.js").then(m => m.default)
 );
 
+const offlineModulePromise = import("../../offline");
 const texturePromise = import("../../rendering/animation").then(m =>
   m.lazyGenerateTextures()
 );
-
 const rendererPromise = getRendererInstance();
+const gamePerquisites = Promise.all([texturePromise, rendererPromise]);
+
+const immedateGameSessionKey = "instantGame";
 
 class PreactService extends Component<Props, State> {
   state: State = {
     dangerMode: false,
-    texturesReady: false,
-    rendererReady: false
+    awaitingGame: false
   };
 
   private _gameChangeSubscribers = new Set<GameChangeCallback>();
+  private _awaitingGameTimeout: number = -1;
+  private _stateService?: Remote<StateService>;
 
   constructor(props: Props) {
     super(props);
     this._init(props);
   }
 
-  render(
-    _props: Props,
-    { game, stateService, dangerMode, texturesReady, rendererReady }: State
-  ) {
+  render(_props: Props, { game, dangerMode, awaitingGame }: State) {
     let mainComponent: VNode;
 
     if (!game) {
-      mainComponent = (
-        <Intro
-          onStartGame={this._onStartGame}
-          loading={!stateService || !texturesReady || !rendererReady}
-        />
-      );
+      if (awaitingGame) {
+        mainComponent = <GameLoading />;
+      } else {
+        mainComponent = <Intro onStartGame={this._onStartGame} />;
+      }
     } else {
       mainComponent = (
         <Game
@@ -92,7 +95,7 @@ class PreactService extends Component<Props, State> {
           toRevealTotal={game.toRevealTotal}
           gameChangeSubscribe={this._onGameChangeSubscribe}
           gameChangeUnsubscribe={this._onGameChangeUnsubscribe}
-          stateService={stateService!}
+          stateService={this._stateService!}
           dangerMode={dangerMode}
           onDangerModeChange={this._onDangerModeChange}
         />
@@ -132,24 +135,49 @@ class PreactService extends Component<Props, State> {
   }
 
   @bind
-  private _onStartGame(width: number, height: number, mines: number) {
-    this.state.stateService!.initGame(width, height, mines);
+  private async _onStartGame(width: number, height: number, mines: number) {
+    const { updateReady, skipWaiting } = await offlineModulePromise;
+
+    if (updateReady) {
+      // There's an update available. Let's load it as part of starting the game…
+      await skipWaiting();
+
+      sessionStorage.setItem(
+        immedateGameSessionKey,
+        JSON.stringify({ width, height, mines })
+      );
+
+      location.reload();
+      return;
+    }
+
+    this._awaitingGameTimeout = setTimeout(() => {
+      this.setState({ awaitingGame: true });
+    }, loadingScreenTimeout);
+
+    // Wait for everything to be ready:
+    await gamePerquisites;
+    const stateService = await this.props.stateServicePromise;
+    stateService.initGame(width, height, mines);
   }
 
-  private async _init(props: Props) {
-    texturePromise.then(() => {
-      this.setState({ texturesReady: true });
-    });
-    rendererPromise.then(() => {
-      this.setState({ rendererReady: true });
-    });
+  private async _init({ stateServicePromise }: Props) {
+    // Is this the reload after an update?
+    const instantGameDataStr = sessionStorage.getItem(immedateGameSessionKey);
 
-    const stateService = await props.stateServicePromise;
-    this.setState({ stateService });
+    if (instantGameDataStr) {
+      sessionStorage.removeItem(immedateGameSessionKey);
+      this.setState({ awaitingGame: true });
+    }
 
-    localStateSubscribe(stateService, stateChange => {
+    offlineModulePromise.then(({ init }) => init());
+
+    this._stateService = await stateServicePromise;
+
+    localStateSubscribe(this._stateService, stateChange => {
       if ("game" in stateChange) {
-        this.setState({ game: stateChange.game });
+        clearTimeout(this._awaitingGameTimeout);
+        this.setState({ game: stateChange.game, awaitingGame: false });
       }
       if ("gameStateChange" in stateChange) {
         for (const callback of this._gameChangeSubscribers) {
@@ -157,6 +185,17 @@ class PreactService extends Component<Props, State> {
         }
       }
     });
+
+    if (instantGameDataStr) {
+      await gamePerquisites;
+      const { width, height, mines } = JSON.parse(instantGameDataStr) as {
+        width: number;
+        height: number;
+        mines: number;
+      };
+
+      this._stateService.initGame(width, height, mines);
+    }
   }
 }
 
