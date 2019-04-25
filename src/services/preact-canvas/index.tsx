f26aee81738aa8fed8db9f2f67dc102b63737c96
Jake Archibald <jaffathecake@gmail.com>
2019-04-25 11:10:25 +0100

    Remembering grid type (and refactorings) (#173)
    * Remembering grid type (and refactorings)
    
    * Remember grid setting in the same session
    
    * Game might be undefined
    
    * Adding board type to the win screen (#180)

--- src/services/preact-canvas/index.tsx
+++ src/services/preact-canvas/index.tsx
@@ -16,7 +16,9 @@ import { Component, h, render, VNode } from "preact";
 import { getBestRenderer } from "src/rendering/renderer";
 import { bind } from "src/utils/bind.js";
 import { StateChange as GameStateChange } from "../../gamelogic";
+import { prerender } from "../../utils/constants";
 import { GameType } from "../state";
+import { getGridDefault, setGridDefault } from "../state/grid-default";
 import StateService from "../state/index.js";
 import localStateSubscribe from "../state/local-state-subscribe.js";
 import BottomBar from "./components/bottom-bar";
@@ -33,12 +35,19 @@ import { game as gameClassName, main } from "./style.css";
 // loading screen?
 const loadingScreenTimeout = 1000;
 
+export interface GridType {
+  width: number;
+  height: number;
+  mines: number;
+}
+
 interface Props {
   stateServicePromise: Promise<Remote<StateService>>;
 }
 
 interface State {
   game?: GameType;
+  gridDefaults?: GridType;
   dangerMode: boolean;
   awaitingGame: boolean;
 }
@@ -63,6 +72,8 @@ const texturePromise = import("../../rendering/animation").then(m =>
 const rendererPromise = getBestRenderer();
 const gamePerquisites = Promise.all([texturePromise, rendererPromise]);
 
+const gridDefaultPromise = getGridDefault();
+
 const immedateGameSessionKey = "instantGame";
 
 class PreactService extends Component<Props, State> {
@@ -80,14 +91,22 @@ class PreactService extends Component<Props, State> {
     this._init(props);
   }
 
-  render(_props: Props, { game, dangerMode, awaitingGame }: State) {
+  render(
+    _props: Props,
+    { game, dangerMode, awaitingGame, gridDefaults }: State
+  ) {
     let mainComponent: VNode;
 
     if (!game) {
       if (awaitingGame) {
         mainComponent = <GameLoading />;
       } else {
-        mainComponent = <Intro onStartGame={this._onStartGame} />;
+        mainComponent = (
+          <Intro
+            onStartGame={this._onStartGame}
+            defaults={prerender ? undefined : gridDefaults}
+          />
+        );
       }
     } else {
       mainComponent = (
@@ -169,6 +188,10 @@ class PreactService extends Component<Props, State> {
   }
 
   private async _init({ stateServicePromise }: Props) {
+    gridDefaultPromise.then(gridDefaults => {
+      this.setState({ gridDefaults });
+    });
+
     // Is this the reload after an update?
     const instantGameDataStr = sessionStorage.getItem(immedateGameSessionKey);
 
@@ -183,8 +206,19 @@ class PreactService extends Component<Props, State> {
 
     localStateSubscribe(this._stateService, stateChange => {
       if ("game" in stateChange) {
-        clearTimeout(this._awaitingGameTimeout);
-        this.setState({ game: stateChange.game, awaitingGame: false });
+        const game = stateChange.game;
+
+        if (game) {
+          clearTimeout(this._awaitingGameTimeout);
+          setGridDefault(game.width, game.height, game.mines);
+          this.setState({
+            game,
+            awaitingGame: false,
+            gridDefaults: game
+          });
+        } else {
+          this.setState({ game });
+        }
       }
       if ("gameStateChange" in stateChange) {
         for (const callback of this._gameChangeSubscribers) {
