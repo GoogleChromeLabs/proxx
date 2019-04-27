d872837e8095a948e33b909b4a381d7a62fc1c75
Mariko Kosaka <mariko.kosaka@gmail.com>
2019-04-27 08:30:18 -0400

    remove width and height from back icon

--- src/services/preact-canvas/components/icons/additional.tsx
+++ src/services/preact-canvas/components/icons/additional.tsx
@@ -47,7 +47,7 @@ export const Close = (props: JSX.HTMLAttributes) => (
 );
 
 export const Back = (props: JSX.HTMLAttributes) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" {...props}>
+  <svg viewBox="0 0 24 24" {...props}>
     <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
   </svg>
 );
