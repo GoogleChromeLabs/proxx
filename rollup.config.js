43d5575a509d609410424c1aff692fa791b3524b
Jake Archibald <jaffathecake@gmail.com>
2019-05-28 17:29:26 +0100

    Preparing for two builds (one prerender)

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,9 @@
 
 import typescript from "rollup-plugin-typescript2";
 import nodeResolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import {
+  terser
+} from "rollup-plugin-terser";
 import loadz0r from "rollup-plugin-loadz0r";
 import dependencyGraph from "./lib/dependency-graph-plugin.js";
 import chunkNamePlugin from "./lib/chunk-name-plugin.js";
@@ -22,112 +24,129 @@ import postcss from "rollup-plugin-postcss";
 import glsl from "./lib/glsl-plugin.js";
 import cssModuleTypes from "./lib/css-module-types.js";
 import assetPlugin from "./lib/asset-plugin.js";
-import { readFileSync } from "fs";
+import {
+  readFileSync
+} from "fs";
 import constsPlugin from "./lib/consts-plugin.js";
 import ejsAssetPlugin from "./lib/ejs-asset-plugin.js";
 import assetTransformPlugin from "./lib/asset-transform-plugin.js";
 import postCSSUrl from "postcss-url";
+import rimraf from 'rimraf';
 
 // Delete 'dist'
-require("rimraf").sync("dist");
+rimraf.sync("dist");
+rimraf.sync("dist-prerender");
+rimraf.sync(".rpt2_cache");
 
-export default {
-  input: {
-    bootstrap: "src/bootstrap.tsx",
-    sw: "src/sw/index.ts"
-  },
-  output: {
-    dir: "dist",
-    format: "amd",
-    sourcemap: true,
-    entryFileNames: "[name].js",
-    chunkFileNames: "[name]-[hash].js"
-  },
-  plugins: [
-    {
-      resolveFileUrl({ fileName }) {
-        return JSON.stringify("/" + fileName);
-      }
+function buildConfig({
+  prerender
+} = {}) {
+  return {
+    input: {
+      bootstrap: "src/bootstrap.tsx",
+      sw: "src/sw/index.ts"
     },
-    cssModuleTypes("src"),
-    postcss({
-      minimize: true,
-      modules: {
-        generateScopedName: "[hash:base64:5]"
-      },
-      namedExports(name) {
-        return name.replace(/-\w/g, val => val.slice(1).toUpperCase());
-      },
-      plugins: [
-        postCSSUrl({
-          url: "inline"
-        })
-      ]
-    }),
-    constsPlugin({
-      version: require("./package.json").version,
-      nebulaSafeDark: require("./lib/nebula-safe-dark").color
-    }),
-    typescript({
-      // Make sure we are using our version of TypeScript.
-      typescript: require("typescript"),
-      tsconfigOverride: {
-        compilerOptions: {
-          sourceMap: true
+    output: {
+      dir: prerender ? "dist-prerender" : "dist",
+      format: "amd",
+      sourcemap: !prerender,
+      entryFileNames: "[name].js",
+      chunkFileNames: "[name]-[hash].js"
+    },
+    plugins: [{
+        resolveFileUrl({
+          fileName
+        }) {
+          return JSON.stringify("/" + fileName);
         }
       },
-      // We need to set this so we can use async functions in our
-      // plugin code. :shrug:
-      // https://github.com/ezolenko/rollup-plugin-typescript2/issues/105
-      clean: true
-    }),
-    glsl(),
-    ejsAssetPlugin("./src/manifest.ejs", "manifest.json", {
-      data: {
-        nebulaSafeDark: require("./lib/nebula-safe-dark").hex
-      }
-    }),
-    assetPlugin({
-      initialAssets: [
-        "./src/assets/space-mono-normal.woff2",
-        "./src/assets/space-mono-bold.woff2",
-        "./src/assets/favicon.png",
-        "./src/assets/social-cover.jpg",
-        "./src/assets/assetlinks.json"
-      ]
-    }),
-    assetTransformPlugin(asset => {
-      if (asset.fileName.includes("manifest-")) {
-        // Remove name hashing
-        asset.fileName = "manifest.json";
-        // Minify
-        asset.source = JSON.stringify(JSON.parse(asset.source));
-      } else if (asset.fileName.includes("assetlinks")) {
-        asset.fileName = ".well-known/assetlinks.json";
-      }
-    }),
-    chunkNamePlugin(),
-    nodeResolve(),
-    loadz0r({
-      loader: readFileSync("./lib/loadz0r-loader.ejs").toString(),
-      // `prependLoader` will be called for every chunk. If it returns `true`,
-      // the loader code will be prepended.
-      prependLoader: (chunk, inputs) => {
-        // If the filename ends with `worker`, prepend the loader.
-        if (
-          Object.keys(chunk.modules).some(mod => /worker\.[jt]s$/.test(mod))
-        ) {
-          return true;
+      !prerender && cssModuleTypes("src"),
+      postcss({
+        minimize: true,
+        modules: {
+          generateScopedName: "[hash:base64:5]"
+        },
+        namedExports(name) {
+          return name.replace(/-\w/g, val => val.slice(1).toUpperCase());
+        },
+        plugins: [
+          postCSSUrl({
+            url: "inline"
+          })
+        ]
+      }),
+      constsPlugin({
+        version: require("./package.json").version,
+        nebulaSafeDark: require("./lib/nebula-safe-dark").color,
+        prerender
+      }),
+      typescript({
+        // Make sure we are using our version of TypeScript.
+        typescript: require("typescript"),
+        tsconfigOverride: {
+          compilerOptions: {
+            sourceMap: true
+          }
+        },
+        // We need to set this so we can use async functions in our
+        // plugin code. :shrug:
+        // https://github.com/ezolenko/rollup-plugin-typescript2/issues/105
+        objectHashIgnoreUnknownHack: true
+      }),
+      glsl(),
+      ejsAssetPlugin("./src/manifest.ejs", "manifest.json", {
+        data: {
+          nebulaSafeDark: require("./lib/nebula-safe-dark").hex
         }
-        // If not, fall back to the default behavior.
-        return loadz0r.isEntryModule(chunk, inputs);
-      }
-    }),
-    dependencyGraph({
-      manifestName: "lib/dependencygraph.json",
-      propList: ["facadeModuleId", "fileName", "imports", "code", "isAsset"]
-    }),
-    resourceListPlugin(),
-    terser()
-  ]
-};
+      }),
+      assetPlugin({
+        initialAssets: [
+          "./src/assets/space-mono-normal.woff2",
+          "./src/assets/space-mono-bold.woff2",
+          "./src/assets/favicon.png",
+          "./src/assets/social-cover.jpg",
+          "./src/assets/assetlinks.json"
+        ]
+      }),
+      assetTransformPlugin(asset => {
+        if (asset.fileName.includes("manifest-")) {
+          // Remove name hashing
+          asset.fileName = "manifest.json";
+          // Minify
+          asset.source = JSON.stringify(JSON.parse(asset.source));
+        } else if (asset.fileName.includes("assetlinks")) {
+          asset.fileName = ".well-known/assetlinks.json";
+        }
+      }),
+      chunkNamePlugin(),
+      nodeResolve(),
+      loadz0r({
+        loader: readFileSync("./lib/loadz0r-loader.ejs").toString(),
+        // `prependLoader` will be called for every chunk. If it returns `true`,
+        // the loader code will be prepended.
+        prependLoader: (chunk, inputs) => {
+          // If the filename ends with `worker`, prepend the loader.
+          if (
+            Object.keys(chunk.modules).some(mod => /worker\.[jt]s$/.test(mod))
+          ) {
+            return true;
+          }
+          // If not, fall back to the default behavior.
+          return loadz0r.isEntryModule(chunk, inputs);
+        }
+      }),
+      !prerender && dependencyGraph({
+        manifestName: "lib/dependencygraph.json",
+        propList: ["facadeModuleId", "fileName", "imports", "code", "isAsset"]
+      }),
+      resourceListPlugin(),
+      !prerender && terser()
+    ].filter(item => item)
+  }
+}
+
+export default [
+  buildConfig({
+    prerender: false
+  }),
+];
