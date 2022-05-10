e6967238cd54570a5330830286abf66ec45440f1
Luca Casonato <hello@lcas.dev>
2022-05-10 23:13:49 +0200

    Add exit fullscreen button
    On some devices, there are no dedicated hardware buttons for leaving
    fullscreen mode (for example browsers on the Steamdeck). This adds a
    software "exit fullscreen" button to the bottom bar.

--- src/main/services/preact-canvas/components/icons/initial.tsx
+++ src/main/services/preact-canvas/components/icons/initial.tsx
@@ -16,9 +16,15 @@ import { h } from "preact";
 
 // tslint:disable:max-line-length variable-name
 
-export const Fullscreen = (props: JSX.HTMLAttributes) => (
+export const FullscreenEnter = (props: JSX.HTMLAttributes) => (
   <svg viewBox="0 0 24 24" {...props}>
     <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
+  </svg>
+);
+
+export const FullscreenExit = (props: JSX.HTMLAttributes) => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path d="M14 14h5v2h-3v3h-2v-5m-9 0h5v5H8v-3H5v-2m3-9h2v5H5V8h3V5m11 3v2h-5V5h2v3h3Z" />
   </svg>
 );
 
